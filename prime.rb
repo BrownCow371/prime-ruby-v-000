313685b4ca6a3f39e31a1b979d68b44856e2f3e8
Jamie Brown <jmbrowncow@gmail.com>
2018-07-02 18:05:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,5 @@
 define prime?(number)
   range_to_check = (2..number-1).to_a
-  range_to_check.find do |range_num|
-    number % range_num == 0
-  end
+  range_to_check.find { |range_num|
+    number % range_num == 0}
 end
