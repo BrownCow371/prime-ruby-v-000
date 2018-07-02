3beaa0055723eb466e1e5c6cc396b47b56093b61
Jamie Brown <jmbrowncow@gmail.com>
2018-07-02 18:12:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
 def prime?(number)
+if number<0 number = number*(-1)
   range_to_check = (2..number-1).to_a
   if range_to_check.find { |range_num|
     number % range_num == 0}
       false
-    else
+  else
       true
-    end
+  end
 end
